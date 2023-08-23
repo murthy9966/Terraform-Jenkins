c025dba408ac9f72c95b4fcbd085dad743645740
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 09:44:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
+  region = "us-east-2" # Change this to your desired AWS region
 }
 
 resource "aws_vpc" "example" {
-  cidr_block = "192.168.0.0/24" # Replace with your desired VPC CIDR block
+  cidr_block = "192.168.0.0/22" # Replace with your desired VPC CIDR block
 
   tags = {
     Name = "example-vpc"
@@ -13,8 +13,8 @@ resource "aws_vpc" "example" {
 resource "aws_subnet" "example_subnet" {
   count           = 2
   vpc_id          = aws_vpc.example.id
-  cidr_block      = "192.168.1.0/22" # Replace with your desired subnet CIDR blocks
-  availability_zone = "us-east-1a" # Replace with your desired availability zones
+  cidr_block      = "192.168.1.0/24" # Replace with your desired subnet CIDR blocks
+  availability_zone = "us-east-2a" # Replace with your desired availability zones
 
   tags = {
     Name = "example-subnet-${count.index + 1}"
