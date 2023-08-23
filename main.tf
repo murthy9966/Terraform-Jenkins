31785386262e6e6b6bbb1b1b180801812055ea50
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 09:50:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,33 @@
 provider "aws" {
-  region = "us-east-2" # Change this to your desired AWS region
+  region = "us-east-1" # Change this to your desired AWS region
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "192.168.0.0/22" # Replace with your desired VPC CIDR block
-
+resource "aws_instance" "example" {
+  ami           = "ami-053b0d53c279acc90" # Replace with your desired AMI ID
+  instance_type = "t2.micro"             # Replace with your desired instance type
+  key_name      = "your-key-pair-name"   # Replace with your SSH key pair name
+  subnet_id     = aws_subnet.example_subnet[0].id # Use the subnet from the VPC example in the previous answer
   tags = {
-    Name = "example-vpc"
+    Name = "example-instance"
   }
 }
 
-resource "aws_subnet" "example_subnet" {
-  count           = 2
-  vpc_id          = aws_vpc.example.id
-  cidr_block      = "192.168.1.0/24" # Replace with your desired subnet CIDR blocks
-  availability_zone = "us-east-2a" # Replace with your desired availability zones
+resource "aws_security_group" "example" {
+  name        = "example-instance-sg"
+  description = "Security group for example EC2 instance"
 
-  tags = {
-    Name = "example-subnet-${count.index + 1}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere (for demonstration purposes)
   }
+
+  # Add more ingress rules as needed for your application
+}
+
+# Associate the security group with the EC2 instance
+resource "aws_network_interface_sg_attachment" "example" {
+  security_group_id    = aws_security_group.example.id
+  network_interface_id = aws_instance.example.network_interface_ids[0]
 }
