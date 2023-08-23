ad72470af827ff44771aa8c0de25ffd6cc977c95
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 11:49:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  # Change this to your desired CIDR block
+  cidr_block = "192.168.0.0/22"  # Change this to your desired CIDR block
 
   tags = {
     Name = "MyVPC"
