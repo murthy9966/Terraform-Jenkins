f127831362012e615e9daea1f61c42f6c8e1758b
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 11:46:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-2"  # Change this to your desired AWS region
-   AWS_ACCESS_KEY_ID = "AKIA3FAEK7GMT6T5CR7L"
-    AWS_SECRET_ACCESS_KEY = "x6skmkHZa+0/6yFo4u0qjY/D9PciHttZfgNRo9ff"
+  region = "us-east-2"  
+   Acess key = "AKIA3FAEK7GMT6T5CR7L"
+   secret key = "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"
 }
 
 resource "aws_vpc" "my_vpc" {
