922d53a3e125b5a619cfee9ea6aefeb03041b563
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 11:40:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,13 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIA3FAEK7GMT6T5CR7L"
-  secret_key = "x6skmkHZa+0/6yFo4u0qjY/D9PciHttZfgNRo9ff"
+  region = "us-east-1"  # Change this to your desired AWS region
+   AWS_ACCESS_KEY_ID = "AKIA3FAEK7GMT6T5CR7L"
+    AWS_SECRET_ACCESS_KEY = "x6skmkHZa+0/6yFo4u0qjY/D9PciHttZfgNRo9ff"
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"  # Specify the desired CIDR block for the VPC
+  cidr_block = "10.0.0.0/16"  # Change this to your desired CIDR block
 
   tags = {
     Name = "MyVPC"
   }
 }
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"  # Specify the CIDR block for the subnet in the VPC
-
-  tags = {
-    Name = "MySubnet"
-  }
-}
-
-resource "aws_security_group" "my_sg" {
-  name_prefix = "MySecurityGroup-"
-  description = "My security group for VPC"
-
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-# Define any additional resources, like route tables, internet gateways, etc. as needed.
