568d231a1c3b6efbbf0071556dd00a6150376c40
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 11:17:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "us-east-1"  # Specify the desired AWS region
+  region     = "us-east-2"
+  access_key = "AKIA3FAEK7GMT6T5CR7L"
+  secret_key = "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"
 }
 
 resource "aws_vpc" "my_vpc" {
