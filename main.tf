a7bf31ec4b53b9fa7cd6c4d59602d53df3c5612b
murthy9966 <124232205+murthy9966@users.noreply.github.com>
2023-08-23 10:50:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,29 @@
 provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
+  region = "us-east-1"  # Specify the desired AWS region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-053b0d53c279acc90" # Replace with your desired AMI ID
-  instance_type = "t2.micro"             # Replace with your desired instance type
-  key_name      = "divya"   # Replace with your SSH key pair name
-  subnet_id     = aws_subnet.example_subnet[0].id # Use the subnet from the VPC example in the previous answer
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"  # Specify the desired CIDR block for the VPC
+
   tags = {
-    Name = "example-instance"
+    Name = "MyVPC"
   }
 }
 
-resource "aws_security_group" "example" {
-  name        = "example-instance-sg"
-  description = "Security group for example EC2 instance"
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"  # Specify the CIDR block for the subnet in the VPC
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from anywhere (for demonstration purposes)
+  tags = {
+    Name = "MySubnet"
   }
-
-  # Add more ingress rules as needed for your application
 }
 
-# Associate the security group with the EC2 instance
-resource "aws_network_interface_sg_attachment" "example" {
-  security_group_id    = aws_security_group.example.id
-  network_interface_id = aws_instance.example.network_interface_ids[0]
+resource "aws_security_group" "my_sg" {
+  name_prefix = "MySecurityGroup-"
+  description = "My security group for VPC"
+
+  vpc_id = aws_vpc.my_vpc.id
 }
+
+# Define any additional resources, like route tables, internet gateways, etc. as needed.
